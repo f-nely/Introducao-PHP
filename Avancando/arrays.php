4ea0c4de78e6744235705b856e2d019e4a72e55b
nely de faria <nelly.nf54@gmail.com>
2020-06-27 12:35:17 -0300

    Include foreach in file arrays.php

--- Avancando/arrays.php
+++ Avancando/arrays.php
@@ -4,3 +4,7 @@ $ageList = [21, 23, 19, 25, 30, 41, 10];
 for ($i = 0; $i < count($ageList); $i++){
     echo $ageList[$i] . PHP_EOL;
 }
+echo "Usando foreach \n";
+foreach ($ageList as $age){
+    echo $age . PHP_EOL;
+}
