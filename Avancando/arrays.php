c58b6e42a08b3b69b8bb8b9c2d03a83fa23992f1
nely de faria <nelly.nf54@gmail.com>
2020-06-27 12:53:23 -0300

    Add item in arrays.php

--- Avancando/arrays.php
+++ Avancando/arrays.php
@@ -1,5 +1,8 @@
 <?php
 $ageList = [21, 23, 19, 25, 30, 41, 10];
+// Add item em um array
+$ageList[] = 80;
+$ageList[8] = 100;
 
 for ($i = 0; $i < count($ageList); $i++){
     echo $ageList[$i] . PHP_EOL;
