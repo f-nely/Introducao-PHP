e761df5307adc8da288cbc89ccf483650f8a5493
nely de faria <nelly.nf54@gmail.com>
2020-06-27 16:55:06 -0300

    Include function list in arrays.php

--- Avancando/arrays.php
+++ Avancando/arrays.php
@@ -3,7 +3,9 @@ $ageList = [21, 23, 19, 25, 30, 41, 10];
 // Add item em um array
 $ageList[] = 80;
 $ageList[8] = 100;
-
+// Function list
+list($ageVinicius, $ageJonh) = $ageList;
+echo "Idade Vinicius é $ageVinicius e Idade John é $ageJonh";
 for ($i = 0; $i < count($ageList); $i++){
     echo $ageList[$i] . PHP_EOL;
 }
