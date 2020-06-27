cf20cac992705dc9c3d45f8849ba3bacaf54ee46
nely de faria <nelly.nf54@gmail.com>
2020-06-27 17:23:45 -0300

    Add comment to file arrays.php

--- Avancando/arrays.php
+++ Avancando/arrays.php
@@ -1,14 +1,23 @@
 <?php
 $ageList = [21, 23, 19, 25, 30, 41, 10];
+
 // Add item em um array
 $ageList[] = 80;
 $ageList[8] = 100;
+
 // Function list
 list($ageVinicius, $ageJonh) = $ageList;
 echo "Idade Vinicius é $ageVinicius e Idade John é $ageJonh";
+
+/*
+ * Another way of use
+ [$ageVinicius, $ageJonh] = $ageList;
+*/
+
 for ($i = 0; $i < count($ageList); $i++){
     echo $ageList[$i] . PHP_EOL;
 }
+
 echo "Usando foreach \n";
 foreach ($ageList as $age){
     echo $age . PHP_EOL;
