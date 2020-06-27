6a65ea22122c95c1720a6be59a9d725728c70499
nely de faria <nelly.nf54@gmail.com>
2020-06-27 12:37:39 -0300

    Adding content to the file conta-corrente.php

--- Avancando/conta-corrente.php
+++ Avancando/conta-corrente.php
@@ -1,1 +1,24 @@
 <?php
+
+$conta1 = [
+    "titular" => "Vinicius",
+    "saldo" => 1000
+];
+$conta2 = [
+    "titular" => "Mary",
+    "saldo" => 15000
+];
+$conta3 = [
+    "titular" => "Harry",
+    "saldo" => 300
+];
+$contaCorrente = [
+    $conta1,
+    $conta2,
+    $conta3
+];
+
+foreach ($contaCorrente as $contas){
+    echo $contas ["titular"] . PHP_EOL;
+}
+
